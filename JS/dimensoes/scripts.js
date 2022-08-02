11ccd1845cfa2a6914be9c69b28b9c3df4af5b34
devkatharine <devkatharine@gmail.com>
2022-08-02 17:49:11 -0300

    navigation menu

--- JS/dimensoes/scripts.js
+++ JS/dimensoes/scripts.js
@@ -5,3 +5,15 @@ function subirTela(){
     behavior: 'smooth'
   });
 }
+
+function botaoScroll (){
+  if (window.screenY === 0){
+    //ocultar botão
+    document.querySelector('.scrollbutton').style.display = 'none';
+  } else {
+    //mostrar o botão
+    document.querySelector('.scrollbutton').style.display = 'block';
+  }
+}
+
+window.addEventListener('scroll', botaoScroll)
