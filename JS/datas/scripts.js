ac3b5ef9e7845ad9e0094d8a085502022f614849
devkatharine <devkatharine@gmail.com>
2022-08-09 11:03:59 -0300

    update date

--- JS/datas/scripts.js
+++ JS/datas/scripts.js
@@ -2,6 +2,23 @@ let data = new Date (2020, 0, 1, 12, 30, 12);
 
 console.log (data.toString());
 
-let dat = new Date ('2021-08-31 21:48:13');
+let dat = new Date ('2021-08-31 21:48:13'); // datas
 
 console.log (dat.toString());
+
+let d = new Date ();
+
+let novoValor = d.getDate(); //data atual
+
+let res = novoValor;
+
+console.log(novoValor);
+
+let hora = d.getHours(); //hora atual
+console.log (hora)
+
+let minuto = d.getMinutes(); //minuto atual
+console.log (minuto)
+
+let seg = d.getSeconds(); //segundo atual
+console.log (seg)
