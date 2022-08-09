352d57b300d6c7aa0b2a3e587ff5680e9c512d87
devkatharine <devkatharine@gmail.com>
2022-08-09 11:27:13 -0300

    math in js

---
+++ JS/matemática/scripts.js
@@ -0,0 +1,11 @@
+let novoValor = Math.round(3.7); // arredondar 
+console.log(novoValor);
+
+let valor = Math.ceil(3.2); // arredondar para cima
+console.log(valor);
+
+let newValor = Math.floor(3.8); // arredondar para baixo
+console.log(newValor);
+
+let numAleatorio = Math.random();
+console.log(numAleatorio)
