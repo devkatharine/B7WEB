5280b4a393b72713e384897c8f10bd4c4d58163b
devkatharine <devkatharine@gmail.com>
2022-08-12 08:25:22 -0300

    arrow function

---
+++ JS/arrowfunction/scripts.js
@@ -0,0 +1,20 @@
+let somar = (x, y) => {
+  return x + y
+}
+
+console.log(somar(10,5))
+
+let sum = (a, b) => a+b;
+
+console.log(sum(5,1))
+
+let letrasNome = (nome) => {
+  return nome.length;
+}
+
+console.log(letrasNome("Katharine"))
+
+let letras = (nome) => nome.length;
+
+console.log(letras("Katharine"))
+
