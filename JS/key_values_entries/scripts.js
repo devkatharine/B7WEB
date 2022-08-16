9c3010e2fd9bc2322a2cb87ef027964eaf217f6b
devkatharine <devkatharine@gmail.com>
2022-08-16 17:01:48 -0300

    values ​​entries and keys in JS

---
+++ JS/key_values_entries/scripts.js
@@ -0,0 +1,12 @@
+let pessoa = {
+  nome: 'Katharine',
+  sobrenome: 'Rabelo',
+  idade: 29
+};
+
+console.log (Object.keys(pessoa)); // saber quantas e quais chaves tem o objeto
+
+console.log (Object.values(pessoa)); // saber os valores das chaves
+
+console.log (Object.entries(pessoa)); // cria um array para cada chave 
+
