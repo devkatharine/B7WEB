fcb308ca7615d0098abf5041cadf143dd4290796
devkatharine <devkatharine@gmail.com>
2022-08-16 14:36:21 -0300

    includes and repeat

---
+++ JS/includes_e_repeat/scripts.js
@@ -0,0 +1,5 @@
+let nome = "Lucca";
+console.log (nome.repeat(5)); //usado para repetição
+
+let lista = ['arroz', 'feijão', 'banana'];
+console.log(lista.includes('morango')); //saber se há algum item dentro de um array ou string, ele irá retornar verdadeiro ou falso.
