3b07847f404017c5126871f3873f6cf5e05114cd
devkatharine <devkatharine@gmail.com>
2022-08-18 15:22:12 -0300

    settimeout in JS

--- JS/callbacks/scripts.js
+++ JS/callbacks/scripts.js
@@ -3,8 +3,13 @@ function alertar (){
 }
 
 let nome = "Katharine";
-setTimeout(alertar, 2000);
+setTimeout(alertar, 2000); //recebe uma função e um tempo para ser executada.
 let sobrenome = "Rabelo";
 console.log(`Nome completo: ${nome} ${sobrenome}`) 
 
 //Função executada depois do alcance de algum resultado.
+//realizar algumas atividades e depois chamar a função de volta 
+
+setTimeout (function(){
+  console.log("depois de 3s")
+}, 3000)
