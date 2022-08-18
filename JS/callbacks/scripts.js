5feec87c8c69b8a49b32b3f5aaf8dd493cfe5d50
devkatharine <devkatharine@gmail.com>
2022-08-18 15:12:13 -0300

    callbacks in JS

---
+++ JS/callbacks/scripts.js
@@ -0,0 +1,10 @@
+function alertar (){
+  console.log("Olá, tudo bem?")
+}
+
+let nome = "Katharine";
+setTimeout(alertar, 2000);
+let sobrenome = "Rabelo";
+console.log(`Nome completo: ${nome} ${sobrenome}`) 
+
+//Função executada depois do alcance de algum resultado.
